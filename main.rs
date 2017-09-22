1bfc133ed18082d83bcae9657076f271f1c76f2a
Shiva <tak2siva@gmail.com>
2017-09-22 09:45:34 +0530

    Command line loop working

--- main.rs
+++ main.rs
@@ -1,23 +1,32 @@
 use std::io;
+use std::io::Write;
+
+fn print_prompt() {
+	print!("sqlite>");
+	io::stdout().flush().unwrap();
+}
+
+fn read_input() -> String {
+	let mut input = String::new();
+	io::stdin().read_line(&mut input).unwrap();
+	return String::from(input.trim());
+}
 
 fn main() {
 	let mut done = false;
 
 	while !done {
-		let mut input = String::new();
-		match io::stdin().read_line(&mut input) {
-		    Ok(_n) => {
-		    	match input.trim().as_ref() {
-		    		"exit" => {
-		    			done = true;
-		    			println!("Bye!");
-		    		}
-		    		_ => {
-		    			println!("{}", input);
-		    		}
-		    	}
-		    }
-		    Err(error) => println!("error: {}", error),
+		print_prompt();
+		let input = read_input();
+
+		match input.as_ref() {
+    		"exit" => {
+    			done = true;
+    			println!("Bye!");
+    		}
+    		_ => {
+    			println!("{}", input);
+    		}
 		}
 	}
 }
